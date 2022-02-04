abad44ceba975f734227c63b6f44d61e856c85c9
giorcocc <giorcocc@gmail.com>
2022-02-04 16:27:43 +0100

    creata la videocamera, le collisioni e i movimenti

--- code/level.py
+++ code/level.py
@@ -2,30 +2,55 @@ import pygame
 from player import Player
 from settings import *
 from tile import Tile
+from debug import debug
 
 
 class Level:
     def __init__(self):
         # get display surface
-        self.display_surface=pygame.display.get_surface()
+        self.display_surface = pygame.display.get_surface()
 
-        #sprite group setup
-        self.visible_sprites=pygame.sprite.Group()
-        self.obstacle_sprites=pygame.sprite.Group()
+        # sprite group setup
+        self.visible_sprites = YSortCameraGroup()
+        self.obstacle_sprites = pygame.sprite.Group()
 
-        #sprite setup
+        # sprite setup
         self.create_map()
 
     def create_map(self):
         for row_index, row in enumerate(WORLD_MAP):
             for col_index, col in enumerate(row):
-                x=col_index*TILESIZE
-                y=row_index*TILESIZE
+                x = col_index * TILESIZE
+                y = row_index * TILESIZE
                 if col == "x":
-                    Tile((x,y),[self.visible_sprites, self.obstacle_sprites])
+                    Tile((x, y), [self.visible_sprites, self.obstacle_sprites])
                 if col == "p":
-                    Player((x,y),[self.visible_sprites])
+                    self.player = Player(
+                        (x, y), [self.visible_sprites], self.obstacle_sprites
+                    )
 
     def run(self):
-        #update and draw the game
-        self.visible_sprites.draw(self.display_surface)
+        # update and draw the game
+        self.visible_sprites.custom_draw(self.player)
+        self.visible_sprites.update()
+        # debug(self.player.direction)
+
+
+class YSortCameraGroup(pygame.sprite.Group):
+    def __init__(self):
+        super().__init__()
+        self.display_surface = pygame.display.get_surface()
+        self.half_width = self.display_surface.get_size()[0] // 2
+        self.half_height = self.display_surface.get_size()[1] // 2
+        self.offset = pygame.math.Vector2()
+
+    def custom_draw(self, player):
+        # getting offset
+        self.offset.x = player.rect.centerx - self.half_width
+        self.offset.y = player.rect.centery - self.half_height
+
+        # for sprite in self.sprites():
+        # nel momento in cui creo la mappa, alcuni blocchi vengono creati dopo il giocatore. Ordino i blocchi in modo che il giocatore, quando si trova sotto un blocco, sia più in avanti
+        for sprite in sorted(self.sprites(), key=lambda sprite: sprite.rect.centery):
+            offset_pos = sprite.rect.topleft - self.offset
+            self.display_surface.blit(sprite.image, offset_pos)
