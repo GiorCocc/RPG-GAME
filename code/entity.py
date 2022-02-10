472022106671c9d0b04e6edaebadfa74abb6f3a2
giorcocc <giorcocc@gmail.com>
2022-02-10 18:34:48 +0100

    interazione con i nemici e il prato

--- code/entity.py
+++ code/entity.py
@@ -1,3 +1,4 @@
+from math import sin
 import pygame
 
 # classe con i metodi in comune tra il giocatore e i nemici
@@ -39,3 +40,8 @@ class Entity(pygame.sprite.Sprite):
                         self.hitbox.bottom = sprite.hitbox.top
                     if self.direction.y < 0:  # movimento verso l'alto
                         self.hitbox.top = sprite.hitbox.bottom
+
+    def wave_value(self):
+        value=sin(pygame.time.get_ticks())
+        if value>=0: return 255
+        else: return 0
--- code/level.py
+++ code/level.py
@@ -21,6 +21,8 @@ class Level:
 
         # attack sprites
         self.current_attack = None
+        self.attack_sprites = pygame.sprite.Group()
+        self.attackable_sprites = pygame.sprite.Group()
 
         # sprite setup
         self.create_map()
@@ -58,7 +60,7 @@ class Level:
                             random_grass_image = choice(graphics["grass"])
                             Tile(
                                 (x, y),
-                                [self.visible_sprites, self.obstacle_sprites],
+                                [self.visible_sprites, self.obstacle_sprites, self.attackable_sprites],
                                 "grass",
                                 random_grass_image,
                             )
@@ -97,8 +99,8 @@ class Level:
                                 Enemy(
                                     monster_name,
                                     (x, y),
-                                    [self.visible_sprites],
-                                    self.obstacle_sprites,
+                                    [self.visible_sprites, self.attackable_sprites],
+                                    self.obstacle_sprites, self.damage_player
                                 )
 
     def create_magic(self, style, strength, cost):
@@ -107,7 +109,7 @@ class Level:
         print(cost)
 
     def create_attack(self):
-        self.current_attack = Weapon(self.player, [self.visible_sprites])
+        self.current_attack = Weapon(self.player, [self.visible_sprites, self.attack_sprites])
 
     def destroy_attack(self):
         if self.current_attack:
@@ -119,8 +121,28 @@ class Level:
         self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
         self.visible_sprites.enemy_update(self.player)
+        self.player_attack_logic()
         self.ui.display(self.player)
         # debug(self.player.status)
+
+    def player_attack_logic(self):
+        if self.attack_sprites:
+            for attack_sprite in self.attack_sprites:
+                collision_sprites=pygame.sprite.spritecollide(attack_sprite, self.attackable_sprites, False)
+                if collision_sprites:
+                    for target_sprite in collision_sprites:
+                        if target_sprite.sprite_type == "grass":
+                            target_sprite.kill()
+                        else:
+                            target_sprite.get_damage(self.player, attack_sprite.sprite_type)
+
+    def damage_player(self, amount, attack_type):
+        if self.player.vulnerable:
+            self.player.health-=amount
+            self.player.vulnerable=False
+            self.player.hurt_time=pygame.time.get_ticks()
+
+            # particelle
 
 
 class YSortCameraGroup(pygame.sprite.Group):
